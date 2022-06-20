59a8766ce9745c8a6941d588311e825bb3da7436
Gabriel Gonzalez <83551041+enderNT@users.noreply.github.com>
2022-06-20 17:24:15 +0000

    ...

---
+++ src/main/java/com/alkemy/backendchallenge/BackendchallengeApplication.java
@@ -0,0 +1,12 @@
+package com.alkemy.backendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendchallengeApplication.class, args);
+	}
+}
